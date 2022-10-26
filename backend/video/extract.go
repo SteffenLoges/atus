13627e552a7d7a82dbe742e0ca04ac21e57c77ea
Steffen Loges <steffen@loges.ovh>
2022-10-26 02:25:22 +0200

    added vsync

--- backend/video/extract.go
+++ backend/video/extract.go
@@ -9,7 +9,7 @@ func (v *ProbedVideo) Extract(s *Stream, newFile string) ([]byte, error) {
 	switch s.CodecType {
 	case "video":
 		// extract video and convert to x264
-		return _exec("ffmpeg", "-y", "-hide_banner", "-loglevel", "warning", "-i", v.File, "-c:v", "libx264", "-an", "-map", fmt.Sprintf("0:%d", s.Index), newFile)
+		return _exec("ffmpeg", "-y", "-hide_banner", "-loglevel", "warning", "-i", v.File, "-c:v", "libx264", "-vsync", "2", "-an", "-map", fmt.Sprintf("0:%d", s.Index), newFile)
 
 	case "audio":
 		// extract audio and convert to aac (ch2)
@@ -26,6 +26,6 @@ func (v *ProbedVideo) Extract(s *Stream, newFile string) ([]byte, error) {
 }
 
 func (v *ProbedVideo) ExtractFrame(timestamp, newFile string) error {
-	_, err := _exec("ffmpeg", "-y", "-hide_banner", "-loglevel", "warning", "-i", v.File, "-ss", timestamp, "-frames:v", "1", "-update", "1", "-q:v", "2", newFile)
+	_, err := _exec("ffmpeg", "-y", "-hide_banner", "-loglevel", "warning", "-i", v.File, "-vsync", "2", "-ss", timestamp, "-frames:v", "1", "-update", "1", "-q:v", "2", newFile)
 	return err
 }
